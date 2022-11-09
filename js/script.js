91b77ba0d72e7361aa6d879afa3b5ebb755986ab
tranquocliem <tranquocliemc6@gmail.com>
2022-11-09 10:07:52 +0700

    Add Button To Top

--- js/script.js
+++ js/script.js
@@ -372,3 +372,28 @@ modalClose.forEach((modalClose) => {
     });
   });
 });
+
+// Srcoll To Top
+// Get the button
+let toTop = document.querySelector(".to_top");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+toTop.addEventListener("click", topFunction);
